2917f31d37271f10572bf910ca9f7fb691cfbb3b
xiangwei.zeng <xiangwei.zeng@agile-robots.com>
2025-07-18 18:15:20 +0800

    开启imu，适配airy

--- launch/start.py
+++ launch/start.py
@@ -1,13 +1,21 @@
+import launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
 
     rviz_config=get_package_share_directory('rslidar_sdk')+'/rviz/rviz2.rviz'
 
-    config_file = '' # your config file path
+    config_file = PathJoinSubstitution(
+        [FindPackageShare("rslidar_sdk"), "config", "config.yaml"]
+    )
+    config_file = config_file.perform(launch.LaunchContext())
     
+    print("config_file: ", config_file)
+
     return LaunchDescription([
         Node(namespace='rslidar_sdk', package='rslidar_sdk', executable='rslidar_sdk_node', output='screen', parameters=[{'config_path': config_file}]),
         Node(namespace='rviz2', package='rviz2', executable='rviz2', arguments=['-d',rviz_config])
